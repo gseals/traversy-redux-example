c3adf5eccd3c05cfa1b3320e470b62a4b849c98a
Gabriel Seals <sealsgj@gmail.com>
2022-03-19 10:49:13 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Posts from './components/Posts';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <Posts />
+      </div>
+    );
+  }
 }
 
 export default App;
